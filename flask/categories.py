1706cb2175f20085537bc95dc88cd5748c4b54d4
Juan Manuel <juan.manuel.neyra@outlook.com>
2020-08-20 17:38:38 -0500

    Fixing bugs

--- flask/categories.py
+++ flask/categories.py
@@ -5,11 +5,6 @@ from flask import Response, jsonify
 categorias=["Ciencias de la Tierra","Economia","Ciencias Politicas","Derecho",
 "Informatica","Astronomia","Literatura Universal","Arte y Diseño"]
 
-categorias2=[{"Cat":"Ciencias de la Tierra"},{"Cat":"Economia"},
-    {"Cat":"Ciencias Politicas"},{"Cat":"Derecho"},
-    {"Cat":"Informatica"},{"Cat":"Astronomia"},{"Cat":"Literatura Universal"},
-    {"Cat":"Arte y Diseño"}]
-
 class categories(Resource):
     def get(self):
         #return {'categorias': [i for i in categorias]}
